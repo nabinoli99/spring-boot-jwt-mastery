d8ccd1a8bde573722d80735e09a44083f543a0e3
Suckscorpio999 <olinabin214@gmail.com>
2026-03-23 18:12:01 +0545

    feat: add RegisterRequestDTO and LoginRequestDTO for auth endpoints

---
+++ src/main/java/com/learn/dto/request/RegisterRequest.java
@@ -0,0 +1,12 @@
+package com.learn.dto.request;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
