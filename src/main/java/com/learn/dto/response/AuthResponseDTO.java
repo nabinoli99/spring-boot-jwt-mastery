8a10f90f51bceb091bd7118026ed802d24def85a
Suckscorpio999 <olinabin214@gmail.com>
2026-03-25 20:41:12 +0545

    refractor : changed AuthResponse to AuthResponseDTO

--- src/main/java/com/learn/dto/response/AuthResponseDTO.java
+++ src/main/java/com/learn/dto/response/AuthResponseDTO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class AuthResponse {
+public class AuthResponseDTO {
     private String token;
     private String email;
     private String role;
