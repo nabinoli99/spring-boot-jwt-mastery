7b7afba2a28988a4ceee707c7bab388e68ed5ddd
Suckscorpio999 <olinabin214@gmail.com>
2026-03-25 20:55:50 +0545

    refractor : changed RegisterRequest to ReqisterRequestDTO

--- src/main/java/com/learn/dto/request/RegisterRequestDTO.java
+++ src/main/java/com/learn/dto/request/RegisterRequestDTO.java
@@ -3,7 +3,7 @@ package com.learn.dto.request;
 import lombok.Data;
 
 @Data
-public class RegisterRequest {
+public class RegisterRequestDTO {
 
     private String firstName;
     private String lastName;
