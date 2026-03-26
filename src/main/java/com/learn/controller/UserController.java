f57ab6d8df817126ea34bf1b0dadbd94d40e8163
Suckscorpio999 <olinabin214@gmail.com>
2026-03-26 09:46:04 +0545

    feat: add AuthController with register and login endpoints

---
+++ src/main/java/com/learn/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.learn.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @GetMapping("/me")
+    public ResponseEntity<String> getMe() {
+        return ResponseEntity.ok("You are authenticated!");
+    }
+}
