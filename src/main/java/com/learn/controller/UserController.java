ef8b3e5a1069ed2dab93db18ac3751e029b92607
Suckscorpio999 <olinabin214@gmail.com>
2026-03-27 15:28:02 +0545

    refactor: update UserController to return real user data using @AuthenticationPrincipal

--- src/main/java/com/learn/controller/UserController.java
+++ src/main/java/com/learn/controller/UserController.java
@@ -1,16 +1,27 @@
 package com.learn.controller;
 
+import com.learn.dto.response.UserResponseDTO;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.learn.entity.User;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
     @GetMapping("/me")
-    public ResponseEntity<String> getMe() {
-        return ResponseEntity.ok("You are authenticated!");
+    public ResponseEntity <UserResponseDTO> getMe(
+            @AuthenticationPrincipal User currentuser) {
+
+        UserResponseDTO dto = new UserResponseDTO(
+                currentuser.getId(),
+                currentuser.getFirstName() + " " + currentuser.getLastName(),
+                currentuser.getEmail(),
+                currentuser.getRole().name()
+                );
+        return ResponseEntity.ok(dto);
     }
 }
